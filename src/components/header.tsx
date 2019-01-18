bc93cf66e88099881a700e09cd1f9fac0c04f5ff
Jari Porola <jari.porola@gofore.com>
2019-01-18 09:41:29 +0200

    enable typescript. convert header component to typescript.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,27 +1,26 @@
 import { Link } from 'gatsby'
-import PropTypes from 'prop-types'
 import React from 'react'
 
-const Header = ({ siteTitle }) => (
+const Header = ({ siteTitle }: Props) => (
   <div
     style={{
       background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
+      marginBottom: `1.45rem`
     }}
   >
     <div
       style={{
         margin: `0 auto`,
         maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
+        padding: `1.45rem 1.0875rem`
       }}
     >
       <h1 style={{ margin: 0 }}>
         <Link
-          to="/"
+          to='/'
           style={{
             color: `white`,
-            textDecoration: `none`,
+            textDecoration: `none`
           }}
         >
           {siteTitle}
@@ -31,12 +30,12 @@ const Header = ({ siteTitle }) => (
   </div>
 )
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
+interface Props {
+  siteTitle?: string
 }
 
 Header.defaultProps = {
-  siteTitle: ``,
+  siteTitle: ``
 }
 
 export default Header

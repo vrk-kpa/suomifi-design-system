bed260469843b9b1e1b282470815ff48ae4315dc
Jari Porola <jari.porola@gmail.com>
2019-01-22 11:08:38 +0200

    remove unnecessary PWA related gatsby plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,21 +16,6 @@ module.exports = {
       }
     },
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
-      }
-    }
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.app/offline
-    // 'gatsby-plugin-offline',
+    `gatsby-plugin-sharp`
   ]
 }

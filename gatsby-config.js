55c3b5b41a6f82d8a721bb86a57cef59f7ab3736
Jari Porola <jari.porola@gmail.com>
2019-01-23 14:24:28 +0200

    update site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`
+    title: `Suomi.fi Design System`,
+    description: `Suomi.fi Design System`,
+    author: `Suomi.fi`
   },
   plugins: [
     `gatsby-plugin-eslint`,

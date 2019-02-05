25321724fa0375721a8807725139a02264f4e9ca
Jari Porola <jari.porola@gmail.com>
2019-02-05 09:46:07 +0200

    add header title with alpha-stamp

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,41 +1,77 @@
 import { Link } from '@wapps/gatsby-plugin-i18next'
 import React from 'react'
+import { NamespacesConsumer } from 'react-i18next'
 import { suomifiTheme } from 'suomifi-ui-components'
 
 import Switcher from './switcher'
 import Logo from './suomifi-logo'
 
 const Header = () => (
-  <div
-    style={{
-      padding: '1rem',
-      borderTop: `4px solid ${suomifiTheme.colors.brandColor}`,
-      boxSizing: `border-box`,
-      background: `${suomifiTheme.colors.white}`,
-      borderBottom: '1px solid #C9CDCF',
-      height: `80px`,
-      display: 'flex',
-      justifyContent: 'center'
-    }}>
-    <div
-      style={{
-        width: '100%',
-        maxWidth: 1140,
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        flexWrap: 'nowrap'
-      }}>
-      <div style={{ height: '32px' }}>
-        <Link to='/'>
-          <Logo />
-        </Link>
+  <NamespacesConsumer>
+    {t => (
+      <div
+        style={{
+          padding: '1rem',
+          borderTop: `4px solid ${suomifiTheme.colors.brandColor}`,
+          boxSizing: `border-box`,
+          background: suomifiTheme.colors.white,
+          borderBottom: '1px solid #C9CDCF',
+          display: 'flex',
+          justifyContent: 'center',
+          lineHeight: '40px'
+        }}>
+        <div
+          style={{
+            width: '100%',
+            maxWidth: 1140,
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            flexWrap: 'wrap'
+          }}>
+          <div style={{ height: '32px' }}>
+            <Link to='/'>
+              <Logo />
+            </Link>
+          </div>
+          <div
+            style={{
+              flex: 1,
+              display: 'flex',
+              flexWrap: 'nowrap',
+              marginLeft: '.5rem'
+            }}>
+            <div
+              style={{
+                fontSize: '28px',
+                fontWeight: 600,
+                color: suomifiTheme.colors.brandColor
+              }}>
+              {t('header:title')}
+            </div>
+            <div
+              style={{
+                whiteSpace: 'nowrap',
+                lineHeight: '14px',
+                alignSelf: 'start',
+                margin: '.5rem',
+                padding: '.2rem .5rem',
+                background: suomifiTheme.colors.secondaryColor,
+                fontSize: '14px',
+                fontWeight: 600,
+                textTransform: 'uppercase',
+                color: suomifiTheme.colors.invertText
+              }}>
+              {t('alpharel:title')}
+            </div>
+          </div>
+          <div>
+            <Switcher />
+          </div>
+        </div>
       </div>
-      <div>
-        <Switcher />
-      </div>
-    </div>
-  </div>
+    )}
+  </NamespacesConsumer>
 )
 
 export default Header

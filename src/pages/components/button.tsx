1a1e893451045cd87ecc378029c79c53da40211f
Jari Porola <jari.porola@gmail.com>
2019-02-18 15:07:42 +0200

    base for button components page

--- src/pages/components/button.tsx
+++ src/pages/components/button.tsx
@@ -2,25 +2,70 @@ import React from 'react'
 import { graphql } from 'gatsby'
 import { NamespacesConsumer } from 'react-i18next'
 import { withI18next } from '@wapps/gatsby-plugin-i18next'
-import { Button } from 'suomifi-ui-components'
+import { suomifiTheme } from 'suomifi-ui-components'
 
 import Layout from '../../components/layout'
 import SEO from '../../components/seo'
-import ComponentExample from '../../components/ComponentExample'
+import { Button } from '../../components/ExampleComponents'
+import ComponentDescription from '../../components/ComponentDescription'
+import sideNavData from '../../config/sidenav/components'
 
 const Page = (): JSX.Element => (
   <NamespacesConsumer ns={['button']}>
     {t => (
-      <Layout>
+      <Layout sideNavData={sideNavData(t)}>
         <SEO title={t('title')} description='' />
         <h1>{t('title')}</h1>
-        <ComponentExample>
-          <Button onClick={() => {}}>{t('click.me')}</Button>
-        </ComponentExample>
+
+        {[
+          { id: 'primary', comp: Button },
+          { id: 'secondary', comp: Button.secondary },
+          { id: 'tertiary', comp: Button.tertiary },
+          {
+            id: 'secondaryNoborder',
+            comp: Button.secondaryNoborder
+          },
+          {
+            id: 'negative',
+            comp: Button.negative,
+            background: true
+          }
+        ].map(item => (
+          <ComponentDescription
+            key={item.id}
+            title={t(`${item.id}.title`)}
+            description={t(`${item.id}.description`)}>
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                flexWrap: 'wrap',
+                padding: '1rem',
+                background: item.background
+                  ? suomifiTheme.colors.secondaryColor
+                  : 'none'
+              }}>
+              <item.comp style={{ margin: '.5rem' }}>
+                {t(`${item.id}.title`)}
+              </item.comp>
+              <item.comp style={{ margin: '.5rem' }} icon='login'>
+                {t('icon.title')}
+              </item.comp>
+              <item.comp style={{ margin: '.5rem' }} iconRight='login'>
+                {t('iconRight.title')}
+              </item.comp>
+              <item.comp style={{ margin: '.5rem' }} disabled>
+                {t('disabled.title')}
+              </item.comp>
+            </div>
+          </ComponentDescription>
+        ))}
       </Layout>
     )}
   </NamespacesConsumer>
 )
+
 export default withI18next()(Page)
 
 export const query = graphql`
--- src/pages/components/index.tsx
+++ src/pages/components/index.tsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 import { NamespacesConsumer } from 'react-i18next'
-import { Link, withI18next } from '@wapps/gatsby-plugin-i18next'
+import { withI18next } from '@wapps/gatsby-plugin-i18next'
 
 import Layout from '../../components/layout'
 import SEO from '../../components/seo'
+import sideNavData from '../../config/sidenav/components'
 
 const Page = (): JSX.Element => (
   <NamespacesConsumer ns={['components']}>
     {t => (
-      <Layout>
+      <Layout sideNavData={sideNavData(t)}>
         <SEO title={t('title')} description='' />
         <h1>{t('title')}</h1>
-        <Link to='/components/button'>{t('button:title')}</Link>
       </Layout>
     )}
   </NamespacesConsumer>

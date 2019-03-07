bd8e65a01679ae99dc4efa93647531723af1fed9
Jari Porola <jari.porola@gmail.com>
2019-03-07 16:44:18 +0200

    use semantic elements for better screen reader support

--- src/components/NoteBox.tsx
+++ src/components/NoteBox.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 
 const NoteBox = ({ title, items }: Props): JSX.Element => (
-  <div
+  <section
     style={{
       background: '#E9F0F8',
       margin: '2rem 0',
       padding: '20px',
       border: '1px solid #C5CBD2'
     }}>
-    <div style={{ fontSize: '22px', fontWeight: 600 }}>{title}</div>
+    <h2 style={{ marginTop: 0, fontSize: '22px', fontWeight: 600 }}>{title}</h2>
     <ul style={{ margin: 0, padding: '0 0 0 2rem' }}>
       {items.map(item => (
         <li key={item} style={{ marginTop: '1rem' }}>
@@ -16,7 +16,7 @@ const NoteBox = ({ title, items }: Props): JSX.Element => (
         </li>
       ))}
     </ul>
-  </div>
+  </section>
 )
 
 interface Props {

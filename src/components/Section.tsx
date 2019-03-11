2d5b3bf27979c9fe16542cfb731739259b3cec8d
Jari Porola <jari.porola@gmail.com>
2019-03-11 14:14:11 +0200

    emptiness check for text contents

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -2,10 +2,13 @@ import React from 'react'
 
 const Section = ({ title, paragraphs }: Props): JSX.Element => (
   <section>
-    <h2>{title}</h2>
-    {paragraphs.map((paragraph, index) => (
-      <p key={index} dangerouslySetInnerHTML={{ __html: paragraph }} />
-    ))}
+    {!!title && <h2>{title}</h2>}
+    {paragraphs.map(
+      (paragraph, index) =>
+        !!paragraph && (
+          <p key={index} dangerouslySetInnerHTML={{ __html: paragraph }} />
+        )
+    )}
   </section>
 )
 

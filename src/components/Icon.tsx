ea608f13d7990f65ef1cdde86551084c1c7f563b
Jari Porola <jari.porola@gmail.com>
2019-04-01 11:41:40 +0300

    use icons from suomifi-ui-components

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -2,16 +2,12 @@ import React, { Component } from 'react'
 
 import SuomiFi from './icons/SuomiFi'
 import Solution from './icons/Solution'
-import CaretDown from './icons/CaretDown'
-import CaretUp from './icons/CaretUp'
 import Favorites from './icons/Favorites'
 import Book from './icons/Book'
 
 export class Icon extends Component {
   public static suomifi = () => <SuomiFi />
   public static solution = () => <Solution />
-  public static caretDown = () => <CaretDown />
-  public static caretUp = () => <CaretUp />
   public static favorites = () => <Favorites />
   public static book = () => <Book />
 }

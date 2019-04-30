403cb70e8d9cdbc13406c942f2ec57b1db1cd761
Jari Porola <jari.porola@gmail.com>
2019-04-30 18:52:58 +0300

    add page for Panel component

--- src/components/ExampleComponents.tsx
+++ src/components/ExampleComponents.tsx
@@ -1,18 +1,8 @@
-import React, { Component } from 'react'
-import { Button as OrigButton, ButtonProps } from 'suomifi-ui-components'
-import { getVariants } from './ExampleComponentUtil'
+import { Button as OrigButton, Panel as OrigPanel } from 'suomifi-ui-components'
+import { addDisplayNames } from './ExampleComponentUtil'
 
-export class Button extends Component<ButtonProps> {
-  public static displayName = 'Button'
+export class Button extends OrigButton {}
+addDisplayNames(Button, OrigButton, 'Button')
 
-  private static variants = getVariants(OrigButton, Button.displayName)
-  public static negative = Button.variants.negative
-  public static secondary = Button.variants.secondary
-  public static secondarySmall = Button.variants.secondarySmall
-  public static secondaryNoborder = Button.variants.secondaryNoborder
-  public static tertiary = Button.variants.tertiary
-
-  public render(): JSX.Element {
-    return <OrigButton {...this.props} />
-  }
-}
+export class Panel extends OrigPanel {}
+addDisplayNames(Panel, OrigPanel, 'Panel')
--- src/config/sidenav/components.js
+++ src/config/sidenav/components.js
@@ -4,5 +4,8 @@ import { Icon } from '../../components/Icon'
 export default t => ({
   title: t('components:title'),
   icon: <Icon.solution />,
-  items: [{ to: '/components/button/', label: t('button:title') }]
+  items: [
+    { to: '/components/button/', label: t('button:title') },
+    { to: '/components/panel/', label: t('panel:title') }
+  ]
 })

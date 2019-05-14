6b807713e2526f8660d6ab8d7340edd920bf35d2
Jari Porola <jari.porola@gmail.com>
2019-05-14 16:16:03 +0300

    improve styles for mobile

--- src/components/Annotation.tsx
+++ src/components/Annotation.tsx
@@ -3,6 +3,7 @@ import { suomifiTheme } from 'suomifi-ui-components'
 
 import { Heading, Text } from './ResponsiveComponents'
 import Link from './Link'
+import { Desktop, Mobile, Tablet } from './Responsive'
 
 const linkStyle = {
   padding: '.5rem 1rem',
@@ -15,6 +16,43 @@ const linkStyle = {
   }
 }
 
+const Content = ({
+  title,
+  description,
+  link,
+  center = false
+}: Props & { center?: boolean }): JSX.Element => (
+  <div
+    style={{
+      width: '100%',
+      maxWidth: 900,
+      display: 'flex',
+      flexWrap: 'wrap',
+      alignItems: 'center',
+      justifyContent: center ? 'center' : 'space-between'
+    }}>
+    <div
+      style={{
+        flex: center ? '1 1 auto' : '1 1 70%',
+        textAlign: center ? 'center' : 'initial'
+      }}>
+      <Heading.h2 color='whiteBase' style={{ textAlign: 'inherit' }}>
+        {title}
+      </Heading.h2>
+      {description && (
+        <p style={{ margin: '1rem 2rem 0 0' }}>
+          <Text color='whiteBase'>{description}</Text>
+        </p>
+      )}
+    </div>
+    {link && !!link.text && !!link.url && (
+      <div style={{ margin: '1.5rem 0 1rem 0' }}>
+        <Link text={link.text} url={link.url} style={linkStyle} />
+      </div>
+    )}
+  </div>
+)
+
 const Annotation = ({ title, description, link }: Props): JSX.Element => (
   <div
     style={{
@@ -24,29 +62,15 @@ const Annotation = ({ title, description, link }: Props): JSX.Element => (
       display: 'flex',
       justifyContent: 'center'
     }}>
-    <div
-      style={{
-        width: '100%',
-        maxWidth: 900,
-        display: 'flex',
-        flexWrap: 'wrap',
-        alignItems: 'center',
-        justifyContent: 'space-between'
-      }}>
-      <div style={{ flex: '1 1 70%' }}>
-        <Heading.h2 color='whiteBase'>{title}</Heading.h2>
-        {description && (
-          <p style={{ margin: '1rem 2rem 0 0' }}>
-            <Text color='whiteBase'>{description}</Text>
-          </p>
-        )}
-      </div>
-      {link && !!link.text && !!link.url && (
-        <div style={{ margin: '1.5rem 0 1rem 0' }}>
-          <Link text={link.text} url={link.url} style={linkStyle} />
-        </div>
-      )}
-    </div>
+    <Desktop>
+      <Content title={title} description={description} link={link} />
+    </Desktop>
+    <Tablet>
+      <Content title={title} description={description} link={link} />
+    </Tablet>
+    <Mobile>
+      <Content title={title} description={description} link={link} center />
+    </Mobile>
   </div>
 )
 

a7342b435c846ec2dff543a8d811b321e607dddc
Jari Porola <jari.porola@gmail.com>
2019-05-14 18:13:54 +0300

    code style

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,14 +1,13 @@
-import React, { ReactNode, CSSProperties } from 'react'
+import React, { ReactNode } from 'react'
 import { NamespacesConsumer } from 'react-i18next'
 import { suomifiTheme } from 'suomifi-ui-components'
-import { Location } from '@reach/router'
 
 import Header from './header'
 import Navigation from './Navigation'
-import SideNavComp from './SideNav'
 import { SideNavData } from './SideNavData'
-import { Desktop, Mobile, Tablet } from './Responsive'
+import { Desktop } from './Responsive'
 import BypassLink from './BypassLink'
+import MainContent from './MainContent'
 import mainNavData from '../config/mainnav'
 
 import '@csstools/normalize.css'
@@ -25,38 +24,6 @@ const BypassLinks = ({ hasSideNav }: { hasSideNav: boolean }): JSX.Element => (
       </>
     )}
   </NamespacesConsumer>
-)
-
-const SideNav = ({
-  sideNavData,
-  style
-}: {
-  sideNavData: SideNavData
-  style?: CSSProperties
-}): JSX.Element => {
-  return sideNavData ? (
-    <Location>
-      {({ location }) => (
-        <div id='sidenav' style={style}>
-          <SideNavComp location={location} sideNavData={sideNavData} />
-        </div>
-      )}
-    </Location>
-  ) : null
-}
-
-const MainContent = ({
-  hasFrame = true,
-  children,
-  style
-}: {
-  hasFrame?: boolean
-  children: ReactNode
-  style?: CSSProperties
-}): JSX.Element => (
-  <main id='main' style={{ margin: hasFrame ? '2rem 1rem' : 0, ...style }}>
-    {children}
-  </main>
 )
 
 const Layout = ({
@@ -78,80 +45,9 @@ const Layout = ({
         <Desktop>
           <Navigation mainNavData={mainNavData(t)} />
         </Desktop>
-        <div
-          style={{
-            background: suomifiTheme.colors.depthLight30,
-            paddingTop: hasFrame ? '1rem' : 0,
-            paddingBottom: '3rem'
-          }}>
-          <Desktop>
-            <div
-              style={{
-                display: 'flex',
-                justifyContent: 'center',
-                flexWrap: 'nowrap'
-              }}>
-              <div
-                style={{
-                  margin: hasFrame ? '1rem 2rem 0 2rem' : 0,
-                  width: '100%',
-                  maxWidth: hasFrame ? 1140 : 'initial',
-                  display: 'flex',
-                  flexWrap: 'nowrap',
-                  background: hasFrame ? suomifiTheme.colors.whiteBase : 'none',
-                  border: hasFrame
-                    ? `1px solid ${suomifiTheme.colors.depthLight13}`
-                    : 0
-                }}>
-                <SideNav
-                  sideNavData={sideNavData}
-                  style={{ width: '22rem', marginRight: '1rem' }}
-                />
-                <MainContent hasFrame={hasFrame} style={{ flex: 1 }}>
-                  {children}
-                </MainContent>
-              </div>
-            </div>
-          </Desktop>
-          <Tablet>
-            <SideNav
-              sideNavData={sideNavData}
-              style={{
-                margin: '0 2rem',
-                border: `1px solid ${suomifiTheme.colors.depthLight13}`
-              }}
-            />
-            <div
-              style={{
-                margin: hasFrame ? '1rem 2rem 0 2rem' : 0,
-                background: hasFrame ? suomifiTheme.colors.whiteBase : 'none',
-                border: hasFrame
-                  ? `1px solid ${suomifiTheme.colors.depthLight13}`
-                  : 0
-              }}>
-              <MainContent hasFrame={hasFrame}>{children}</MainContent>
-            </div>
-          </Tablet>
-          <Mobile>
-            <SideNav
-              sideNavData={sideNavData}
-              style={{
-                margin: '0 1rem',
-                border: `1px solid ${suomifiTheme.colors.depthLight13}`
-              }}
-            />
-            <div
-              style={{
-                margin: hasFrame ? '1rem 0 0 0' : 0,
-                background: hasFrame ? suomifiTheme.colors.whiteBase : 'none',
-                border: hasFrame
-                  ? `1px solid ${suomifiTheme.colors.depthLight13}`
-                  : 0
-              }}>
-              <MainContent hasFrame={hasFrame}>{children}</MainContent>
-            </div>
-          </Mobile>
-        </div>
+        <MainContent sideNavData={sideNavData} hasFrame={hasFrame}>
+          {children}
+        </MainContent>
       </div>
     )}
   </NamespacesConsumer>

2d6c3364967aed6645faceb44d65629c6037f4c3
Jari Porola <jari.porola@gmail.com>
2019-06-05 15:17:14 +0300

    code style

---
+++ src/components/ResponsiveComponentsUtil.tsx
@@ -0,0 +1,34 @@
+import React, { ComponentClass } from 'react'
+
+import { Desktop, MobileOrTablet } from 'components/Responsive'
+
+export const getResponsiveComponent = (Comp: Function): Function => {
+  const Wrapper = (props): JSX.Element => (
+    <>
+      <Desktop>
+        <Comp {...props} />
+      </Desktop>
+      <MobileOrTablet>
+        <Comp smallScreen {...props} />
+      </MobileOrTablet>
+    </>
+  )
+
+  return Wrapper
+}
+
+const getVariants = (Comp: ComponentClass): Record<string, Function> =>
+  Object.keys(Comp)
+    .map(key => ({
+      [key]: getResponsiveComponent(Comp[key])
+    }))
+    .reduce((obj, item) => ({ ...obj, ...item }), {})
+
+export const addResponsiveness = (
+  DestComp: ComponentClass,
+  SourceComp: ComponentClass
+): void => {
+  Object.assign(DestComp, {
+    ...getVariants(SourceComp)
+  })
+}

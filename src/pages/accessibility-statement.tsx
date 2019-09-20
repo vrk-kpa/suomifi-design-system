9bca25770dce22c65c966062ac5b5c24ce031787
Aappo Ålander <aappo.alander@gofore.com>
2019-09-20 13:54:41 +0300

    Accessibility statement page and footer link added.

---
+++ src/pages/accessibility-statement.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+import { NamespacesConsumer } from 'react-i18next';
+import { withI18next } from '@wapps/gatsby-plugin-i18next';
+import { suomifiTheme } from 'suomifi-ui-components';
+
+import Layout from 'components/layout';
+import SEO from 'components/seo';
+import Section from 'components/Section';
+import { Heading } from 'components/ResponsiveComponents';
+import Link from 'components/Link';
+
+const Page = (): JSX.Element => (
+  <NamespacesConsumer ns={['accessibility-statement']}>
+    {t => (
+      <Layout>
+        <div style={{ padding: `0 ${suomifiTheme.spacing.l}` }}>
+          <SEO title={t('title')} />
+          <Heading variant="h1">{t('title')}</Heading>
+          {t('sections').map((section, index) => (
+            <Section
+              key={index}
+              mainTitle={section.title}
+              paragraphs={section.paragraphs}
+              links={section.links}
+            />
+          ))}
+        </div>
+      </Layout>
+    )}
+  </NamespacesConsumer>
+);
+
+export default withI18next()(Page);
+
+export const query = graphql`
+  query($lng: String!) {
+    ...AllLocalesFragment
+  }
+`;

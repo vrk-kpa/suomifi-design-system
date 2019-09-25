d6bce2e5b491632cb51f1c48d535fb0df84f8849
LJKaski <joonas.kaski@gofore.com>
2019-09-25 11:08:10 +0300

    Initial commit with the page and texts in place

---
+++ src/pages/components/breadcrumb.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+import { NamespacesConsumer } from 'react-i18next';
+import { withI18next } from '@wapps/gatsby-plugin-i18next';
+import { suomifiTheme } from 'suomifi-ui-components';
+
+import Layout from 'components/layout';
+import SEO from 'components/seo';
+import { Panel } from 'components/ExampleComponents';
+import ComponentDescription from 'components/ComponentDescription';
+import sideNavData from 'config/sidenav/components';
+import NoteBox from 'components/NoteBox';
+import Section from 'components/Section';
+import ComponentExample from 'components/ComponentExample';
+import { Heading, Text, Paragraph } from 'components/ResponsiveComponents';
+
+const Page = (): JSX.Element => (
+  <NamespacesConsumer ns={['breadcrumb']}>
+    {t => (
+      <Layout sideNavData={sideNavData(t)}>
+        <SEO title={t('title')} />
+        <Heading.h1>{t('title')}</Heading.h1>
+
+        <Paragraph.lead>
+          <Text.lead>{t('intro')}</Text.lead>
+        </Paragraph.lead>
+
+        <NoteBox title={t('note.title')} items={t('note.items')} />
+
+        {t('sections').map((section, index) => (
+          <Section
+            key={index}
+            mainTitle={section.title}
+            paragraphs={section.paragraphs}
+            links={section.links}
+          />
+        ))}
+      </Layout>
+    )}
+  </NamespacesConsumer>
+);
+
+export default withI18next()(Page);
+
+export const query = graphql`
+  query($lng: String!) {
+    ...AllLocalesFragment
+  }
+`;

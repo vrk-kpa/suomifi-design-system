17e3a03c419d2f519f79f5662cfb4ce6fb614cf9
LJKaski <joonas.kaski@gofore.com>
2019-09-25 13:31:38 +0300

    Add basic example and sidenav link. Proofread content

--- src/pages/components/breadcrumb.tsx
+++ src/pages/components/breadcrumb.tsx
@@ -2,11 +2,10 @@ import React from 'react';
 import { graphql } from 'gatsby';
 import { NamespacesConsumer } from 'react-i18next';
 import { withI18next } from '@wapps/gatsby-plugin-i18next';
-import { suomifiTheme } from 'suomifi-ui-components';
 
 import Layout from 'components/layout';
 import SEO from 'components/seo';
-import { Panel } from 'components/ExampleComponents';
+import { Breadcrumb } from 'components/ExampleComponents';
 import ComponentDescription from 'components/ComponentDescription';
 import sideNavData from 'config/sidenav/components';
 import NoteBox from 'components/NoteBox';
@@ -35,6 +34,21 @@ const Page = (): JSX.Element => (
             links={section.links}
           />
         ))}
+
+        <ComponentDescription
+          mainTitle={t('default.title')}
+          description={t('default.description')}
+        >
+          <ComponentExample>
+            <Breadcrumb aria-label="breadcrumb">
+              <Breadcrumb.link href="/">{t('default.link1')}</Breadcrumb.link>
+              <Breadcrumb.link href="/alasivu">
+                {t('default.link2')}
+              </Breadcrumb.link>
+              <Breadcrumb.link current>{t('default.link3')}</Breadcrumb.link>
+            </Breadcrumb>
+          </ComponentExample>
+        </ComponentDescription>
       </Layout>
     )}
   </NamespacesConsumer>

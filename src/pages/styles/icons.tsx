990928ad919b6ef8a216e956730d73a3340b8a61
Aappo Ålander <aappo.alander@gofore.com>
2019-10-14 15:22:54 +0300

    Illustrative icons added and groupings improved.

--- src/pages/styles/icons.tsx
+++ src/pages/styles/icons.tsx
@@ -4,7 +4,13 @@ import styled from 'styled-components';
 import { NamespacesConsumer } from 'react-i18next';
 import { withI18next } from '@wapps/gatsby-plugin-i18next';
 import { suomifiTheme } from 'suomifi-ui-components';
-import { IconKeys, allIcons, SuomifiIcon } from 'suomifi-icons';
+import {
+  allIcons,
+  IconKeys,
+  allStaticIcons,
+  StaticIconKeys,
+  SuomifiIcon,
+} from 'suomifi-icons';
 
 import Layout from 'components/layout';
 import SEO from 'components/seo';
@@ -15,84 +21,14 @@ import ComponentExample from 'components/ComponentExample';
 import { Heading, Text, Paragraph } from 'components/ResponsiveComponents';
 import { ReactComponent as Slack } from 'icons/slack.svg';
 
-interface IconItem {
-  name: IconKeys;
-}
-type iconTypes = { [key in IconKeys]?: IconItem };
-
-const icons: iconTypes = allIcons.reduce((obj, item: string) => {
-  obj[item] = {
-    name: item,
-  };
-  return obj;
-}, {});
-
 const iconCategories = [
   {
     id: 'baseIcons',
-    icons: [
-      icons.arrowDown,
-      icons.arrowUp,
-      icons.attachment,
-      icons.authorise,
-      icons.calendar,
-      icons.chatHeart,
-      icons.chatQuestion,
-      icons.check,
-      icons.checkFilled,
-      icons.chevronDown,
-    ],
+    icons: allIcons,
   },
   {
     id: 'illustrativeIcons',
-    icons: [
-      icons.chevronLeft,
-      icons.chevronRight,
-      icons.chevronUp,
-      icons.close,
-      icons.copy,
-      icons.done,
-      icons.error,
-      icons.errorFilled,
-      icons.heart,
-      icons.heartFilled,
-    ],
-  },
-  {
-    id: 'doctypeIcons',
-    icons: [
-      icons.help,
-      icons.helpFilled,
-      icons.info,
-      icons.infoFilled,
-      icons.internet,
-      icons.linkBreadcrumb,
-      icons.linkExternal,
-      icons.linkList,
-      icons.login,
-      icons.logout,
-      icons.mailSend,
-      icons.map,
-      icons.mapLayers,
-      icons.mapLocation,
-      icons.mapLocationFilled,
-      icons.mapRoute,
-      icons.menu,
-      icons.message,
-      icons.minus,
-      icons.phone,
-      icons.plus,
-      icons.print,
-      icons.radioButtonOn,
-      icons.refresh,
-      icons.registers,
-      icons.remove,
-      icons.save,
-      icons.search,
-      icons.settings,
-      icons.swapRounded,
-      icons.swapVertical,
-    ],
+    icons: allStaticIcons.filter(icon => icon !== 'toggle'),
   },
 ];
 const StyleIcon = styled(SuomifiIcon)`
@@ -101,7 +37,7 @@ const StyleIcon = styled(SuomifiIcon)`
 `;
 
 const getExampleIcon = (
-  id: IconKeys,
+  id: IconKeys | StaticIconKeys,
   label: string,
   style?: CSSProperties,
 ): JSX.Element => (
@@ -152,8 +88,8 @@ const Page = (): JSX.Element => (
                 background: 'none',
               }}
             >
-              {item.icons.map((icon, index) => {
-                return getExampleIcon(icon.name, t(`${icon.name}.label`));
+              {item.icons.map(icon => {
+                return getExampleIcon(icon, t(`${icon}.label`));
               })}
             </ComponentExample>
           </ComponentDescription>

fac08952b6fa1bf8489dabed277e10e568f673f0
LJKaski <joonas.kaski@gofore.com>
2019-11-29 12:17:25 +0200

    Add notification box component

---
+++ src/components/NotificationBox.tsx
@@ -0,0 +1,45 @@
+import React, { ReactNode, CSSProperties } from 'react';
+import { suomifiTheme } from 'suomifi-ui-components';
+import { Text } from 'components/ResponsiveComponents';
+import { Icon } from 'suomifi-ui-components';
+import { NamespacesConsumer } from 'react-i18next';
+
+interface Props {
+  style?: CSSProperties;
+}
+
+const NotificationBox = ({ style }: Props): JSX.Element => (
+  <NamespacesConsumer>
+    {t => (
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          flexWrap: 'wrap',
+          padding: suomifiTheme.spacing.l,
+          margin: `${suomifiTheme.spacing.m} 0`,
+          ...style,
+          background: suomifiTheme.colors.accentSecondaryLight40,
+          fontSize: '24px',
+        }}
+      >
+        <Icon icon="error" color={suomifiTheme.colors.accentSecondary} />
+        <Text.bold
+          style={{
+            marginLeft: suomifiTheme.spacing.m,
+            verticalAlign: 'middle',
+            fontSize: suomifiTheme.typography.fontSize.body,
+          }}
+        >
+          {' '}
+          {t('common:workInProgressWarning')}
+        </Text.bold>
+      </div>
+    )}
+  </NamespacesConsumer>
+);
+
+NotificationBox.displayName = 'div';
+
+export default NotificationBox;

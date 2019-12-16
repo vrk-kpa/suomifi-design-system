bc446d2d2d2f511ebbe00b066a830c175a29d226
Sami Korpela <ketsappi@users.noreply.github.com>
2019-12-16 12:58:54 +0200

    Fix example to use panelShadow like before

--- src/examples/styles.js
+++ src/examples/styles.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { suomifiDesignTokens } from 'suomifi-ui-components';
+import { panelShadow } from '../components/utils/shadow';
 
 export const Example = () => (
   <div
@@ -7,6 +8,7 @@ export const Example = () => (
       margin: suomifiDesignTokens.spacing.l,
       padding: suomifiDesignTokens.spacing.m,
       background: suomifiDesignTokens.colors.highlightBase,
+      boxShadow: panelShadow,
       fontSize:
         suomifiDesignTokens.values.typography.bodyText.fontSize.value +
         suomifiDesignTokens.values.typography.bodyText.fontSize.unit,

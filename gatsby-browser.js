caffa0348463336d3378611153c66806ca557aa9
Aappo Ålander <aappo.alander@gofore.com>
2020-02-20 13:27:02 +0200

    Global styles moved to gatsby-browser.js as require.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,5 @@
+require('suomifi-ui-components/dist/main.css');
 exports.onClientEntry = () => {
   // cleanup detected language at least for now when availableLngs evolves
-  typeof window !== 'undefined' && window.localStorage.removeItem('@wappsLng')
-}
+  typeof window !== 'undefined' && window.localStorage.removeItem('@wappsLng');
+};

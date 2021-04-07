e902876566f02a267a52cc09e6d2df89dfd885be
LJKaski <joonas.kaski@gofore.com>
2021-04-07 15:36:29 +0300

    update custom focus indicator to look more like that in the library

--- src/components/utils/outline.ts
+++ src/components/utils/outline.ts
@@ -12,12 +12,12 @@ export const focusOutline = `
       left: -4px;
       background-color: transparent;
       box-sizing: border-box;
-      box-shadow: ${suomifiDesignTokens.colors.accentBase} 0px 0px 10px 0px;
+      box-shadow: ${suomifiDesignTokens.colors.accentSecondary} 0px 0px 0px 0px;
       z-index: 9999;
       border-radius: 4px;
-      border-width: 1px;
+      border-width: 3px;
       border-style: solid;
-      border-color: ${suomifiDesignTokens.colors.accentBase} ;
+      border-color: ${suomifiDesignTokens.colors.accentSecondary} ;
       border-image: initial;
     }
   `;

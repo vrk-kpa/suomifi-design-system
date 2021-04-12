846fcef516a5154f5420a2abda442d26591f0f3c
Sami Korpela <ketsappi@users.noreply.github.com>
2021-04-12 13:24:42 +0300

    Remove the Orig-version of components that are no longer used

--- src/components/ExampleComponents.tsx
+++ src/components/ExampleComponents.tsx
@@ -1,43 +1,19 @@
 import {
-  Button as OrigButton,
-  Expander as OrigExpander,
   Breadcrumb as OrigBreadcrumb,
-  ToggleButton as OrigToggleButton,
-  ToggleInput as OrigToggleInput,
-  Dropdown as OrigDropdown,
   Link as OrigLink,
   Heading as OrigHeading,
   Text as OrigText,
-  TextInput as OrigTextInput,
   SearchInput as OrigSearchInput,
   Icon as OrigIcon,
   StaticIcon as OrigStaticIcon,
   Block as OrigBlock,
   LanguageMenu as OrigLanguageMenu,
   LanguageMenuItem as OrigLanguageMenuItem,
-  Checkbox as OrigCheckbox,
-  RadioButton as OrigRadioButton,
-  Textarea as OrigTextarea,
 } from 'suomifi-ui-components';
 import { addDisplayNames } from 'components/ExampleComponentUtil';
 
-// export class Button extends OrigButton {}
-// addDisplayNames(Button, OrigButton, 'Button');
-
-export class Expander extends OrigExpander {}
-addDisplayNames(Expander, OrigExpander, 'Expander');
-
 export class Breadcrumb extends OrigBreadcrumb {}
 addDisplayNames(Breadcrumb, OrigBreadcrumb, 'Breadcrumb');
-
-// export class ToggleButton extends OrigToggleButton {}
-// addDisplayNames(ToggleButton, OrigToggleButton, 'ToggleButton');
-
-// export class ToggleInput extends OrigToggleInput {}
-// addDisplayNames(ToggleInput, OrigToggleInput, 'ToggleInput');
-
-// export class Dropdown extends OrigDropdown {}
-// addDisplayNames(Dropdown, OrigDropdown, 'Dropdown');
 
 export class Link extends OrigLink {}
 addDisplayNames(Link, OrigLink, 'Link');
@@ -47,9 +23,6 @@ addDisplayNames(Heading, OrigHeading, 'Heading');
 
 export class Text extends OrigText {}
 addDisplayNames(Text, OrigText, 'Text');
-
-// export class TextInput extends OrigTextInput {}
-// addDisplayNames(TextInput, OrigText, 'TextInput');
 
 export class SearchInput extends OrigSearchInput {}
 addDisplayNames(SearchInput, OrigSearchInput, 'SearchInput');
@@ -68,12 +41,3 @@ addDisplayNames(LanguageMenu, OrigLanguageMenu, 'LanguageMenu');
 
 export class LanguageMenuItem extends OrigLanguageMenuItem {}
 addDisplayNames(LanguageMenuItem, OrigLanguageMenuItem, 'LanguageMenuItem');
-
-// export class Checkbox extends OrigCheckbox {}
-// addDisplayNames(Checkbox, OrigCheckbox, 'Checkbox');
-
-// export class RadioButton extends OrigRadioButton {}
-// addDisplayNames(RadioButton, OrigRadioButton, 'RadioButton');
-
-// export class Textarea extends OrigTextarea {}
-// addDisplayNames(OrigTextarea, 'Textarea');

b82c71c1f03321e8fbe939416da4dab7f42b6a09
Sami Korpela <ketsappi@users.noreply.github.com>
2021-11-18 13:47:32 +0200

    Fix Heading pages to use correct content .json

--- src/pages/components/heading.tsx
+++ src/pages/components/heading.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import headingContent from '../../../locale/fi/dropdown.json';
+import headingContent from '../../../locale/fi/heading.json';
 import Layout from 'components/layout';
 import SEO from 'components/seo';
 import sideNavData from 'config/sidenav/components';

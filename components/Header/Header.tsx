0fbc34895dcac57612e2347a650a24cac3bd3274
Sami Korpela <ketsappi@users.noreply.github.com>
2022-07-08 12:12:09 +0300

    Add mobileNav menu logic; no actual interaction yet

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,19 +1,32 @@
-import Link from "next/link";
-import { Block } from "suomifi-ui-components";
-import styles from "./Header.module.scss";
-import Navbar from "./Navbar/Navbar";
+import Link from 'next/link';
+import { Block, Icon } from 'suomifi-ui-components';
+import styles from './Header.module.scss';
+import Navbar from './Navbar/Navbar';
 
 const Header: React.FunctionComponent = () => {
   return (
     <Block variant="header" className={styles.header}>
-      <Block className="container">
-        <Link href="/">
-          <img
-            src="/designSystemLogo.svg"
-            alt="Suomi.fi Design System"
-            className={styles.logo}
-          />
-        </Link>
+      <Block className={styles.upper}>
+        <Block className={styles.container}>
+          <Link href="/">
+            <img
+              src="/designSystemLogo.svg"
+              alt="Suomi.fi Design System"
+              className={styles.logo}
+            />
+          </Link>
+          <div className={styles.mobileNavContainer}>
+            {/** TODO: Refactor to own component */}
+            <button className={styles.mobileMenuButton}>
+              <Icon
+                className={styles.mobileMenuIcon}
+                icon="menu"
+                variant="secondaryNoBorder"
+                aria-label="Avaa paanavigaatio"
+              />
+            </button>
+          </div>
+        </Block>
       </Block>
       <div className={styles.desktopNavContainer}>
         <Navbar />

29b35a9149b06d4b3c785ae5b7709cc4e6ce6e45
Sami Korpela <ketsappi@users.noreply.github.com>
2022-07-08 12:49:35 +0300

    Change mobile nav menu icon when toggling

--- components/Header/MobileNavMenuButton/MobileNavMenuButton.tsx
+++ components/Header/MobileNavMenuButton/MobileNavMenuButton.tsx
@@ -1,12 +1,19 @@
+import React, { useState } from 'react';
 import { Icon } from 'suomifi-ui-components';
 import styles from './MobileNavMenuButton.module.scss';
 
 const MobileNavMenuButton: React.FunctionComponent = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const clickHandler = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
-    <button className={styles.mobileMenuButton}>
+    <button className={styles.mobileMenuButton} onClick={clickHandler}>
       <Icon
         className={styles.mobileMenuIcon}
-        icon="menu"
+        icon={menuOpen ? 'close' : 'menu'}
         variant="secondaryNoBorder"
         aria-label="Avaa paanavigaatio"
       />

f76cbb8cbdacc2bb714177ba3e43d6721de95ae9
Sami Korpela <ketsappi@users.noreply.github.com>
2022-07-11 15:19:53 +0300

    Add functionality to mobile nav menu

--- components/Header/MobileNavMenuButton/MobileNavMenuButton.tsx
+++ components/Header/MobileNavMenuButton/MobileNavMenuButton.tsx
@@ -1,6 +1,31 @@
-import React, { useState } from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import React from 'react';
+import classnames from 'classnames';
 import { Icon } from 'suomifi-ui-components';
 import styles from './MobileNavMenuButton.module.scss';
+
+/**
+ * TODO: getting items from props etc.
+ */
+const navItems = [
+  {
+    title: 'Etusivu',
+    path: '/',
+  },
+  {
+    title: 'Tyylit',
+    path: '/styles',
+  },
+  {
+    title: 'Komponentit',
+    path: '/components',
+  },
+  {
+    title: 'Info',
+    path: '/info',
+  },
+];
 
 interface MobileNavMenuButtonProps {
   /** For screenreader to read */
@@ -12,25 +37,63 @@ const MobileNavMenuButton: React.FunctionComponent<MobileNavMenuButtonProps> = (
   props,
 ) => {
   const { ariaLabel } = props;
-  const [menuOpen, setMenuOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = React.useState(false);
+  const router = useRouter();
 
   const clickHandler = () => {
+    console.log('mobileNaV clickandler:', menuOpen);
     setMenuOpen(!menuOpen);
   };
 
+  // If the current path is only partially the current one. For e.g when at /components/button.
+  const isPartiallyActive = (to: string, pathName: string) => {
+    return (
+      to &&
+      pathName.substring(1).startsWith(to.substring(1)) &&
+      to != pathName &&
+      to.length > 1
+    );
+  };
+
   return (
-    <button
-      className={styles.mobileMenuButton}
-      onClick={clickHandler}
-      aria-label={ariaLabel}
-      aria-expanded={menuOpen}
-      aria-haspopup={true}
-    >
-      <Icon
-        className={styles.mobileMenuIcon}
-        icon={menuOpen ? 'close' : 'menu'}
-      />
-    </button>
+    <div className={styles.wrapper}>
+      <button
+        className={styles.button}
+        onClick={clickHandler}
+        aria-label={ariaLabel}
+        aria-expanded={menuOpen}
+        aria-haspopup={true}
+      >
+        <Icon className={styles.icon} icon={menuOpen ? 'close' : 'menu'} />
+      </button>
+      {menuOpen && (
+        <div className={styles.menu}>
+          <nav>
+            <ul>
+              {navItems.map((item) => (
+                <li
+                  key={item.title}
+                  className={classnames(
+                    { [styles.active]: router.pathname === item.path },
+                    {
+                      [styles.partiallyActive]: isPartiallyActive(
+                        item.path,
+                        router.pathname,
+                      ),
+                    },
+                  )}
+                  aria-current={
+                    router.pathname === item.path ? 'page' : undefined
+                  }
+                >
+                  <Link href={item.path}>{item.title.toUpperCase()}</Link>
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </div>
+      )}
+    </div>
   );
 };
 

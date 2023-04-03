15bd6c35df7e1c2db0f57cc3f9cd224051205b92
Jenni Unkuri <jenni.unkuri@gofore.com>
2023-04-03 10:58:08 +0300

    Add base path to nextjs config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: process.env.BASE_PATH ? process.env.BASE_PATH : '',
   images: {
     unoptimized: true,
   },
